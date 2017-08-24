38a655e3309ec11defe3bd4f4d9a2c3a1595df77
Sachin B <mailme@sachinbhatnagar.com>
2017-08-24 19:26:01 +0530

    Lesson 01 - End

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 // Your VueJS code goes here
+const vm = new Vue({
+    el: '#app',
+    data() {
+        return {
+            greeting: 'Hello!'
+        }
+    }
+});
+
+vm.greeting = "Hey there!";
