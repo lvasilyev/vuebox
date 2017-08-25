71a0c19b207b695dd3279ed85d367737e279b24d
Sachin B <mailme@sachinbhatnagar.com>
2017-08-26 01:01:30 +0530

    Lesson 3.0 Start

--- js/app.js
+++ js/app.js
@@ -1,80 +1,21 @@
 // Your VueJS code goes here
 new Vue({
     el: '#app',
-    data() {
-        return {
-            title: 'Sign-in to your account',
-            buttonText: 'Sign-in'
-        }
-    },
+    template: `
+        <div id="number-box">
+            <ul>
+                <li v-for="x in 30" :style="backgroundColor()" @click="rerender">{{x < 10 ? ('0' + x) : x}}</li>
+            </ul>
+        </div>
+    `,
     methods: {
-        signupTitle() {
-            this.title = 'Signup for a new account!';
-            this.buttonText = 'Signup';
-        }
-    },
-    render(createElement) {
-        return createElement('div', {
-            attrs: {
-                id: 'login-dialog'
+        rerender() {
+            this.$forceUpdate();
+        },
+        backgroundColor() {
+            return {
+                'background-color': `rgb(${parseInt(Math.random() * 255)}, ${parseInt(Math.random() * 255)}, ${parseInt(Math.random() * 255)})`
             }
-        }, [
-            createElement('div', {
-                attrs: {
-                    id: 'login-title'
-                }
-            }, [ this.title ]),
-            createElement('div', {
-                attrs: {
-                    class: 'input-holder'
-                }
-            }, [
-                createElement('input', {
-                    attrs: {
-                        type: 'text',
-                        placeholder: 'E-Mail'
-                    }
-                })
-            ]),
-            createElement('div', {
-                attrs: {
-                    class: 'input-holder'
-                }
-            }, [
-                createElement('input', {
-                    attrs: {
-                        type: 'password',
-                        placeholder: 'Password'
-                    }
-                })
-            ]),
-            createElement('div', {
-                attrs: {
-                    class: 'input-holder'
-                }
-            }, [
-                createElement('a', {
-                    attrs: {
-                        href: '#',
-                        id: 'signinBtn'
-                    }
-                }, [ this.buttonText ]),
-                createElement('div', {
-                    attrs: {
-                        class: 'text-links'
-                    }
-                }, [
-                    createElement('a', {
-                        attrs: {
-                            href: '#',
-                            id: 'signupBtn'
-                        },
-                        on: {
-                            click: this.signupTitle
-                        }
-                    }, [ 'Signup for a free account!' ])
-                ])
-            ])
-        ])
+        }
     }
 });
