9c978d97807896408ffa258435e9ea1f9a79e09a
Sachin B <mailme@sachinbhatnagar.com>
2017-08-25 20:13:35 +0530

    Lesson 04 - End

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 // Your VueJS code goes here
 let template = '<div id="login-dialog"><div id="login-title">{{ title }}</div><div class="input-holder"><input type="text" placeholder="E-Mail"></div><div class="input-holder"><input type="password" placeholder="Password"></div><div class="input-holder"><a href="#" id="signinBtn">Sign-in</a><div class="text-links"><a href="#" id="signupBtn">Signup for a free account!</a></div></div></div>';
 
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            title: 'Sign-in to your account'
+        }
+    },
+    template
+});
