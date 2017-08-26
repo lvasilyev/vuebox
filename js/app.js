c0aa70120bf0b6f0452f5407b230fcc38bfc8dbe
Sachin B <mailme@sachinbhatnagar.com>
2017-08-26 18:17:52 +0530

    Lesson 3.1 - Start

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
+const products = [
+    {
+        product: "Ben & Jerry's Coconut Seven",
+        cost: 30,
+        quantity: 2
+    }, {
+        product: "Lipton Honey Lemon Green Tea",
+        cost: 10,
+        quantity: 6
+    }, {
+        product: "Lindt Lindor - Milk Truffles",
+        cost: 100,
+        quantity: 1
+    }, {
+        product: "Ferrero Rocher (24 Pieces)",
+        cost: 12,
+        quantity: 4
+    }, {
+        product: "Goodwyn Tea",
+        cost: 20,
+        quantity: 8
+    }, {
+        product: "Typhoo Treasures Green Tea",
+        cost: 5,
+        quantity: 10
+    }, {
+        product: "Quaker Oats",
+        cost: 30,
+        quantity: 2
+    }
+];
 // Your VueJS code goes here
+new Vue({
+    el: '#app'
+});
