dd6f5263950a037f6b95ed3e73f3ca73fa37817d
Sachin B <mailme@sachinbhatnagar.com>
2017-08-27 23:42:25 +0530

    Lesson 4.0 - Start

--- js/app.js
+++ js/app.js
@@ -1,34 +1,6 @@
 // Your VueJS code goes here
 new Vue({
-    el: '#app',
-    data() {
-        return {
-            product: '',
-            cart: []
-        }
-    },
-    methods: {
-        addProduct() {
-            if(this.product !== '') {
-                this.cart.push(this.product);
-                this.product = '';
-            }
-        },
-        removeItem(key) {
-            this.cart.splice(key, 1);
-        }
-    },
-    template: `
-        <div class="add-products-container">
-            <div class="add-products">
-                <ul>
-                    <li class="product" v-for="(item, key) in cart">
-                        <span class="title">{{item}}</span>
-                        <a href="#" class="remove-btn" @click.prevent="removeItem(key)">X</a>
-                    </li>
-                    <li class="search-box"><input type="text" placeholder="Add Products..." v-model="product" @keyup.enter="addProduct"></li>
-                </ul>
-            </div>
-        </div>
-    `
+    el: '#app'
 });
+
+
