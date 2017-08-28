652fe56d4e657cfe8e72ecc50e96b4c420b5b9e0
Sachin B <mailme@sachinbhatnagar.com>
2017-08-28 15:12:24 +0530

    Lesson 4.0 - Start

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
+const dataSet = [{
+    id: 1,
+    name: 'Mocha',
+    type: ['American', 'Mexican', 'Patisserie', 'Cafe'],
+    rating: 4.3
+}, {
+    id: 2,
+    name: 'Playground',
+    type: ['American', 'Italian', 'Indian'],
+    rating: 3.8
+}, {
+    id: 3,
+    name: 'MTV Flyp',
+    type: ['Cafe', 'Pub'],
+    rating: 4.0
+}, {
+    id: 5,
+    name: 'F Bar',
+    type: ['Bar', 'Cafe', 'American'],
+    rating: 4.0
+}];
 // Your VueJS code goes here
 new Vue({
     el: '#app'
 });
-
-
