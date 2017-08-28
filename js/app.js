2dcb1e2f53f1fc9a03b3fb1b1de9a462ddb91769
Sachin B <mailme@sachinbhatnagar.com>
2017-08-29 00:56:12 +0530

    Lesson 4.1 - End

--- js/app.js
+++ js/app.js
@@ -1,80 +1,49 @@
-const places = [
-    {
-        id: 1,
-        name: 'Mocha',
-        type: ['American', 'Mexican', 'Patisserie', 'Cafe'],
-        rating: 4.3
-    }, {
-        id: 2,
-        name: 'Playground',
-        type: ['American', 'Italian', 'Indian'],
-        rating: 1.2
-    }, {
-        id: 3,
-        name: 'MTV Flyp',
-        type: ['Cafe', 'Pub'],
-        rating: 3.0
-    }, {
-        id: 5,
-        name: 'F Bar',
-        type: ['Bar', 'Cafe', 'American'],
-        rating: 4.0
-    }
-];
 // Your VueJS code goes here
-// const RestaurantName = {
-//     name: 'restaurant-name',
-//     props: ['name'],
-//     template: `<div class="restaurant-name">{{name}}</div>`
-// }
-
-Vue.component('restaurant-name', {
-    functional: true,
-    render(createElement, context) {
-        return createElement('div', {
-            attrs: {
-                class: 'restaurant-name'
-            }
-        }, context.props.name);
+const InputField = {
+    name: 'input-field',
+    props: ['value'],
+    template: '<input type="number" min="0" :value="value" @keyup="update">',
+    methods: {
+        update() {
+            this.$emit('update:value', Number(event.target.value));
+        }
     }
-});
-
-const RestaurantType = {
-    name: 'restaurant-type',
-    props: ['type'],
-    template: `<div class="restaurant-type">{{type.join(' | ')}}</div>`
 }
 
-const ListItem = {
-    name: 'list-item',
-    props: ['listData'],
-    components: {
-        RestaurantType
-    },
-    template: 
-        `<div class="list-item">
-            <restaurant-name :name="listData.name" />
-            <restaurant-type :type="listData.type" />
-         </div>`
+const RangeSlider = {
+    name: 'range-slider',
+    props: ['value'],
+    template: `<input type="range" min="0" max="100" :value="value" @input="onInput">`,
+    methods: {
+        onInput() {
+            this.$emit('update:value', Number(event.target.value));
+        }
+    }
 }
 
-new Vue({
-    el: '#app',
+
+const App = {
+    name: 'app',
     data() {
         return {
-            places
+            val: 0
         }
     },
     components: {
-        ListItem
+        InputField,
+        RangeSlider
     },
-    template: `
-        <div id="list-container">
-            <list-item 
-                v-for="place in places" 
-                :key="place.id" 
-                :list-data="place" 
-            />
-        </div>
-    `
+    template: 
+        `<div id="container">
+            <input-field :value.sync="val" />
+            <range-slider :value.sync="val" />
+        </div>`
+}
+
+const vm = new Vue({
+    el: '#app',
+    components: {
+        App
+    },
+    template: '<App />'
 });
