cda50f96aa5207b07dc4bb7cb8dfcb466be149da
Sachin B <mailme@sachinbhatnagar.com>
2017-09-08 23:45:05 +0530

    Lesson 3.2 - End

--- js/app.js
+++ js/app.js
@@ -1,34 +1,58 @@
 // Your VueJS code goes here
-new Vue({
-    el: '#app',
+const ListWidget = {
+    props: ['value'],
     data() {
         return {
             product: '',
-            cart: []
+            products: this.value
         }
     },
     methods: {
         addProduct() {
             if(this.product !== '') {
-                this.cart.push(this.product);
+                this.products.push(this.product);
                 this.product = '';
+                this.syncCart();
             }
         },
         removeItem(key) {
-            this.cart.splice(key, 1);
+            this.products.splice(key, 1);
+            this.syncCart();
+        },
+        syncCart() {
+            this.$emit('input', this.products);
+        }
+    },
+    template: `
+    <div class="add-products">
+        <ul>
+            <li class="product" v-for="(item, key) in products">
+                <span class="title">{{item}}</span>
+                <a href="#" class="remove-btn" @click.prevent="removeItem(key)">X</a>
+            </li>
+            <li class="search-box"><input type="text" placeholder="Add Products..." v-model="product" @keyup.enter="addProduct"></li>
+        </ul>
+    </div>
+    `
+}
+
+
+new Vue({
+    el: '#app',
+    components: {
+        ListWidget
+    },
+    data() {
+        return {
+            cart: []
         }
     },
     template: `
         <div class="add-products-container">
-            <div class="add-products">
-                <ul>
-                    <li class="product" v-for="(item, key) in cart">
-                        <span class="title">{{item}}</span>
-                        <a href="#" class="remove-btn" @click.prevent="removeItem(key)">X</a>
-                    </li>
-                    <li class="search-box"><input type="text" placeholder="Add Products..." v-model="product" @keyup.enter="addProduct"></li>
-                </ul>
-            </div>
+            <ListWidget v-model="cart" />
+            <ol class="products-list">
+                <li v-for="item in cart">{{item}}</li>
+            </ol>
         </div>
     `
 });
