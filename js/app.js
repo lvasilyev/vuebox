37687be5ad1a27f432c88ee5dd39f34b43b1227c
Sachin B <mailme@sachinbhatnagar.com>
2017-09-09 00:21:32 +0530

    Lesson 3.1 - End

--- js/app.js
+++ js/app.js
@@ -26,55 +26,55 @@ const products = [
     }
 ];
 // Your VueJS code goes here
-
 const ListItem = {
+    name: 'list-item',
     props: ['item'],
     template: `
     <span>
         <span class="product">{{item.product}}</span>
-        <span class="quantity">{{item.quantity}}</span>
-        <span class="cost">{{item.quantity * item.cost}}</span>
+        <span class="quantity"><input type="number" v-model="item.quantity"></span>
+        <span class="cost">{{item.cost * item.quantity}}</span>
     </span>
     `
 }
 
 const App = {
+    name: 'app',
+    data() {
+        return {
+            cart: products
+        }
+    },
     components: {
         ListItem
     },
-    data() {
-        return {
-            cart: products,
-            total: 0
-        }
-    },
-    template: `
-        <div id="cart">
-            <h2>Shopping List</h2>
-            <ol id="list">
-                <li v-for="item in cart">
-                    <ListItem :item="item" />
-                </li>
-            </ol>
-
-            <div id="totals">
-                Total = USD {{totals}}
-            </div>
-        </div>
-    `,
     computed: {
         totals() {
             return this.cart.reduce(function(total, item) {
                 return total + (item.cost * item.quantity);
             }, 0);
         }
-    }
-};
+    },
+    template: `
+    <div id="cart">
+        <h2>Shopping List</h2>
+        <ol id="list">
+            <li v-for="item in cart">
+                <list-item :item="item" />
+            </li>
+        </ol>
+
+        <div id="totals">
+            Total = USD {{totals}}
+        </div>
+    </div>
+    `
+}
 
 new Vue({
     el: '#app',
     components: {
         App
     },
-    template: '<App />'
+    template: '<app />'
 });
