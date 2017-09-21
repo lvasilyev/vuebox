efd16845b0fd418684ece61e9ec8702d2fa6d67a
Sachin B <mailme@sachinbhatnagar.com>
2017-09-21 19:02:31 +0530

    Lesson 4.3 - End

--- js/app.js
+++ js/app.js
@@ -1,32 +1,74 @@
 // Your VueJS code goes here
-new Vue({
-    el: '#app',
+Vue.component('slides', {
+    props: ['src'],
     data() {
         return {
-            slidevalue: 0
-        }
-    },
-    created() {
-        this.runshow();
-    },
-    methods: {
-        slide() {
-            if(this.slidevalue > -800) {
-                this.slidevalue = this.slidevalue - 320;
-            }
-        },
-        runshow() {
-            setInterval(() => {
-                this.slide();
-            }, 4000);
-        }
+            width: this.$parent.w,
+            height: this.$parent.h
+        }   
     },
     computed: {
-        translateVal() {
+        getStyle() {
             return {
-                'transform': `translate3d(${this.slidevalue}px, 0px, 0px)`,
-                'transition-duration': 1000 + 'ms'
+                'background-image': `url(${this.src})`,
+                width: `${this.width}px`,
+                height: `${this.height}px`
             }
         }
-    }
+    },
+    template: '<div class="slides" :style="getStyle"></div>'
+});
+
+Vue.component('slideshow', {
+    props: ['w','h'],
+    data() {
+        return {
+            galleryXPos: 0,
+            slideCount: 0,
+            maxSlide: 0
+        }
+    },
+    mounted() {
+        this.getSlideCount();
+        setInterval(this.runShow, 3000);
+    },
+    methods: {
+        runShow() {
+            if(this.galleryXPos !== this.maxSlide) {
+                this.galleryXPos = this.galleryXPos - this.w;
+            } else {
+                this.galleryXPos = 0;
+            }
+        },
+        getSlideCount() {
+            this.slideCount = this.$slots.default.filter(item => {
+                return item.elm.className === 'slides'
+            }).length;
+
+            this.maxSlide = -(this.w * (this.slideCount - 1));
+        },
+        getViewportSize() {
+            return {
+                width: `${this.w}px`,
+                height: `${this.h}px`
+            }
+        },
+        getGallerySize() {
+            return {
+                'width': `${this.slideCount * this.w}px`,
+                'transform': `translate(${this.galleryXPos}px, 0px)`
+            }
+        }
+    },
+    template: `
+    <div class="viewport" :style="getViewportSize()">
+        <div class="gallery" :style="getGallerySize()">
+            <slot></slot>
+        </div>
+   </div>
+    `
+})
+
+new Vue({
+    el: '#app'
 });
