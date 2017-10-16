e02a9c0636ffba82e7d7d24ea040c4a1dde23b51
Sachin B <mailme@sachinbhatnagar.com>
2017-10-16 12:36:47 +0530

    Lesson 5.0 - End

---
+++ js/app.js
@@ -0,0 +1,40 @@
+// Your VueJS code goes here...
+Vue.component('warn', {
+    props: ['show'],
+    template:
+    `
+    <transition name="warn-transition">
+        <div class="warning" v-if="show">
+            <span class="warn-text">
+                <slot name="message">Processing Successful!</slot>
+            </span>
+            <span class="warn-records">
+                <strong><slot name="records"></slot> records were updated!</strong>
+            </span>
+            <a href="#" class="cancel-btn" @click="$emit('close')">X</a>
+        </div>
+    </transition>
+    `
+});
+
+Vue.component('app', {
+    data() {
+        return {
+            showWarning: false
+        }
+    },
+    template: 
+    `
+    <div>
+        <a href="#" class="remove-btn" @click="showWarning = true">Remove Data!</a>
+        <warn :show="showWarning" @close="showWarning = false">
+            <span slot="message">Your data has been removed from the system!</span>
+            <span slot="records">234</span>
+        </warn>        
+    </div>`
+})
+
+new Vue({
+    el: '#app',
+    template: '<app />'
+});
